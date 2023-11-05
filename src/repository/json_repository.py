92ed119f89ebe8f6670e5d471b8b0d882bfb5d3c
austinibele <austin.ibele@kungfu.ai>
2023-11-05 13:56:14 +0100

    JsonRepository class that uses TinyDB + Tests

--- src/repository/json_repository.py
+++ src/repository/json_repository.py
@@ -3,35 +3,26 @@ from tinydb import TinyDB, Query
 class JsonRepository:
     def __init__(self, filename: str):
         self.db = TinyDB(filename)
-        self.prompt_chain_table = self.db.table('prompt_chain')  # Create a table for prompt chains
 
     def save_prompt_chain(self, prompt_chain_data: dict) -> None:
         # Insert a new document into the table
-        self.prompt_chain_table.insert(prompt_chain_data)
+        self.db.insert(prompt_chain_data)
 
     def get_prompt_chain(self, chain_id: str) -> dict:
         # Search for a document by chain_id
         PromptChain = Query()
-        return self.prompt_chain_table.search(PromptChain.chain_id == chain_id)
+        return self.db.search(PromptChain.chain_id == chain_id)
 
     def update_prompt_chain(self, chain_id: str, updated_data: dict) -> None:
         # Update document matched by chain_id
         PromptChain = Query()
-        self.prompt_chain_table.update(updated_data, PromptChain.chain_id == chain_id)
+        self.db.update(updated_data, PromptChain.chain_id == chain_id)
 
     def delete_prompt_chain(self, chain_id: str) -> None:
         # Remove document by chain_id
         PromptChain = Query()
-        self.prompt_chain_table.remove(PromptChain.chain_id == chain_id)
+        self.db.remove(PromptChain.chain_id == chain_id)
 
     def list_prompt_chains(self) -> list:
         # Return all documents in the table
-        return self.prompt_chain_table.all()
-
-# Example usage
-# db = JsonDB('prompt_chains.json')
-# db.save_prompt_chain(prompt_chain_data)
-# chain = db.get_prompt_chain('chain_id')
-# db.update_prompt_chain('chain_id', updated_data)
-# db.delete_prompt_chain('chain_id')
-# all_chains = db.list_prompt_chains()
+        return self.db.all()
--- tests/repository/test_json_repository.py
+++ tests/repository/test_json_repository.py
@@ -9,33 +9,31 @@ def prompt_chain_data():
     return data
 
 @pytest.fixture()
-def db_instance(tmp_path):
-    # Create a temporary database file
-    db_file = tmp_path / "test_prompt_chains.json"
-    db = JsonRepository(filename=db_file)
+def repository_instance():
+    db = JsonRepository(filename="tests/repository/prompt_chain_fixture.json")
     return db
 
-def test_save_prompt_chain(db_instance, prompt_chain_data):
-    db_instance.save_prompt_chain(prompt_chain_data)
-    assert len(db_instance.list_prompt_chains()) == 1
+def test_save_prompt_chain(repository_instance, prompt_chain_data):
+    repository_instance.save_prompt_chain(prompt_chain_data)
+    assert len(repository_instance.list_prompt_chains()) == 1
 
-def test_get_prompt_chain(db_instance, prompt_chain_data):
-    chain = db_instance.get_prompt_chain(prompt_chain_data["chain_id"])
+def test_get_prompt_chain(repository_instance, prompt_chain_data):
+    chain = repository_instance.get_prompt_chain(prompt_chain_data["chain_id"])
     assert chain[0]["chain_id"] == prompt_chain_data["chain_id"]
 
-def test_update_prompt_chain(db_instance, prompt_chain_data):
+def test_update_prompt_chain(repository_instance, prompt_chain_data):
     updated_version = "1.0.1"
     prompt_chain_data["version"] = updated_version
-    db_instance.update_prompt_chain(prompt_chain_data["chain_id"], prompt_chain_data)
-    chain = db_instance.get_prompt_chain(prompt_chain_data["chain_id"])
+    repository_instance.update_prompt_chain(prompt_chain_data["chain_id"], prompt_chain_data)
+    chain = repository_instance.get_prompt_chain(prompt_chain_data["chain_id"])
     assert chain[0]["version"] == updated_version
 
-def test_delete_prompt_chain(db_instance, prompt_chain_data):
-    db_instance.delete_prompt_chain(prompt_chain_data["chain_id"])
-    chain = db_instance.get_prompt_chain(prompt_chain_data["chain_id"])
+def test_delete_prompt_chain(repository_instance, prompt_chain_data):
+    repository_instance.delete_prompt_chain(prompt_chain_data["chain_id"])
+    chain = repository_instance.get_prompt_chain(prompt_chain_data["chain_id"])
     assert not chain
 
-def test_list_prompt_chains(db_instance, prompt_chain_data):
-    db_instance.save_prompt_chain(prompt_chain_data)
-    assert isinstance(db_instance.list_prompt_chains(), list)
-    db_instance.delete_prompt_chain(prompt_chain_data["chain_id"])  # Clean up after test
+def test_list_prompt_chains(repository_instance, prompt_chain_data):
+    repository_instance.save_prompt_chain(prompt_chain_data)
+    assert isinstance(repository_instance.list_prompt_chains(), list)
+    repository_instance.delete_prompt_chain(prompt_chain_data["chain_id"])  # Clean up after test
