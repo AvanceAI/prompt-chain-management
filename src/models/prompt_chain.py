dffa1a2381b1d1a870068b8164b18a8c29a7ee30
austinibele <austin.ibele@kungfu.ai>
2023-11-05 14:10:13 +0100

    Place Prompt and PromptChain models in same file

--- src/models/prompt_chain.py
+++ src/models/prompt_chain.py
@@ -1,6 +1,14 @@
 from typing import List
 from pydantic import BaseModel, Field
-from src.models.prompt import Prompt
+
+class Prompt(BaseModel):
+    prompt_id: str = Field(..., description="Unique identifier for the prompt.")
+    prompt_text: str = Field(..., description="The text of the prompt to be sent.")
+    response_type: str = Field(..., description="Expected response type.", enum=["text", "json"])
+    dependencies: List[str] = Field(default_factory=list, description="List of dependencies that this prompt relies on.")
+    parameters: dict = Field(default_factory=dict, description="Parameters that can be passed into the prompt.")
+    user_interaction: dict = Field(default_factory=dict, description="Details about user interaction if required.")
+    external_service: str = Field(None, description="Name of external service to integrate with the prompt.")
 
 class PromptChain(BaseModel):
     chain_id: str = Field(..., description="Unique identifier for the prompt chain.")
