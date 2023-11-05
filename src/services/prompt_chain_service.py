cf91bc23816ed86fd6549f5540d9a39f14fa34cf
austinibele <austin.ibele@kungfu.ai>
2023-11-05 17:09:42 +0100

    Tests for PromptChainService

--- src/services/prompt_chain_service.py
+++ src/services/prompt_chain_service.py
@@ -1,23 +1,23 @@
-from models.prompt_chain import PromptChain
-from db.json_db import JsonDB
+from src.models.prompt_chain import PromptChain
+from src.repository.json_repository import JsonRepository
 
 class PromptChainService:
-    def __init__(self, db: JsonDB):
-        self.db = db
+    def __init__(self, repository: JsonRepository):
+        self.repository = repository 
 
     def create_prompt_chain(self, prompt_chain_data: dict) -> PromptChain:
         prompt_chain = PromptChain(**prompt_chain_data)
         # Save to the database
-        self.db.save_prompt_chain(prompt_chain.dict())
+        self.repository.save_prompt_chain(prompt_chain.dict())
         return prompt_chain
 
     def execute_prompt_chain(self, chain_id: str) -> None:
         # Retrieve the prompt chain from the database
-        prompt_chain_data = self.db.get_prompt_chain(chain_id)
-        if not prompt_chain_data:
+        prompt_chain_data = self.repository.get_prompt_chain(chain_id)
+        
+        if not prompt_chain_data or len(prompt_chain_data) == 0:
             raise ValueError("Prompt chain not found")
-
-        prompt_chain = PromptChain(**prompt_chain_data)
+        prompt_chain = PromptChain(**prompt_chain_data[0])
         
         # Here you would add the logic to execute each prompt in the chain
         # This would involve iterating over the prompt_chain.prompts list
---
+++ tests/services/test_prompt_chain_service.py
@@ -0,0 +1,57 @@
+import pytest
+from src.services.prompt_chain_service import PromptChainService
+from src.repository.json_repository import JsonRepository
+
+# Fixture to mock the database file interaction
+@pytest.fixture
+def mock_repository(tmp_path):
+    repository_file = tmp_path / "test_prompt_chain.json"
+    repository_file.write_text('{}')  # Initialize the file with an empty JSON object
+    return JsonRepository(filename=str(repository_file))
+
+# Test the creation of a prompt chain
+def test_create_prompt_chain(mock_repository):
+    service = PromptChainService(repository=mock_repository)
+    prompt_chain_data = {
+        "chain_id": "test_chain",
+        "chain_title": "Test Chain",
+        "prompts": [
+            {
+                "prompt_id": "test_prompt_1",
+                "prompt_text": "This is a test prompt.",
+                "response_type": "text"
+            }
+        ]
+    }
+    prompt_chain = service.create_prompt_chain(prompt_chain_data)
+    assert prompt_chain.chain_id == prompt_chain_data["chain_id"]
+    assert len(prompt_chain.prompts) == len(prompt_chain_data["prompts"])
+
+# Test the retrieval and execution of a prompt chain
+def test_execute_prompt_chain(mock_repository):
+    service = PromptChainService(repository=mock_repository)
+    prompt_chain_data = {
+        "chain_id": "test_chain_execute",
+        "chain_title": "Test Chain for Execution",
+        "prompts": [
+            {
+                "prompt_id": "test_prompt_exec_1",
+                "prompt_text": "Execute this test prompt.",
+                "response_type": "text"
+            }
+        ]
+    }
+    service.create_prompt_chain(prompt_chain_data)
+
+    # You should ideally mock the execution response as well
+    service.execute_prompt_chain("test_chain_execute")
+    # Add assertions here to check if the prompt was executed correctly
+    # For example, check the state of the database if the responses are stored
+
+# Test the error handling when the prompt chain does not exist
+def test_execute_nonexistent_prompt_chain(mock_repository):
+    service = PromptChainService(repository=mock_repository)
+    with pytest.raises(ValueError) as excinfo:
+        service.execute_prompt_chain("nonexistent_chain")
+    assert "Prompt chain not found" in str(excinfo.value)
+
